4d377152e2e59b86754ae1ab2a7413adf2fb1e0a
James Keats <growlitheharpo@gmail.com>
2018-08-04 15:06:21 -0400

    I'm silly.

--- Program.cs
+++ Program.cs
@@ -2,17 +2,22 @@
 
 namespace WallpaperManager
 {
-    class Program
+	internal class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int mode = -1;
-            Console.WriteLine("MENU\n\t 1. Create a new database\n");
-            Console.WriteLine("MENU\n\t 2. Run query and copy files\n");
+	        Console.WriteLine("MENU\n");
+	        Console.WriteLine("\t 1. Create a new database\n");
+            Console.WriteLine("\t 2. Run query and copy files\n");
             Console.Write("Your choice: ");
-            string input = Console.ReadLine();
-            mode = int.Parse(input);
+	        
+	        string input = null;
+		    while (input == null ) 
+			    input = Console.ReadLine();
+            
+	        mode = int.Parse(input);
 
             switch (mode)
             {
@@ -21,8 +26,6 @@ namespace WallpaperManager
                     break;
                 case 2:
                     FindFiles.DoFindFiles();
-                    break;
-                default:
                     break;
             }
         }
