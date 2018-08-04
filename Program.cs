0e7baf49504641262f84e07963db9e23451b2060
James Keats <growlitheharpo@gmail.com>
2018-08-04 15:01:36 -0400

    Actually add the other option to the main menu.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace WallpaperManager
         {
             int mode = -1;
             Console.WriteLine("MENU\n\t 1. Create a new database\n");
+            Console.WriteLine("MENU\n\t 2. Run query and copy files\n");
             Console.Write("Your choice: ");
             string input = Console.ReadLine();
             mode = int.Parse(input);
